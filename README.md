f52815e5035265785d48d5dcbd74026135c85a14
alextanhongpin <alextan@seekasia.com>
2018-04-19 13:47:58 +0800

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+## Main Network
+
+Starting the geth network:
+
+```
+$ geth --datadir "./tmp" \
+--syncmode "fast" \
+--cache 1024 \
+--rpc \
+--rpcaddr="localhost" \
+--rpcport="8545" \
+--rpccorsdomain="http://localhost:3000" \
+--networkid 1 # This refers to the main network
+```
+
+The `--rpccorsdomain` points to our local nodejs server that is serving the web3 script on port `3000`.
+
+
+## Test Network
+
+To connect to the test network, just specify this when running `geth`:
+
+```bash
+--testnet    Ropsten network: pre-configured proof-of-work test network
+--rinkeby    Rinkeby network: pre-configured proof-of-authority test network
+```
+
+## Run Client
+
+```bash
+$ node server.js
+```
---
+++ public/index.js
@@ -0,0 +1,34 @@
+(function (Web3) {
+  'use strict'
+
+  const ERC20_CONTRACT = ''
+
+  const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+
+  if (web3.isConnected()) {
+    console.log('connected')
+  }
+  const balance = web3.eth.getBalance(ERC20_CONTRACT)
+  console.log('balance:', balance)
+
+  const block = web3.eth.getBlock('earliest') // earliest, latest, pending
+  console.log('blockNumber:', block, web3.eth.blockNumber)
+  console.log('syncing:', web3.eth.syncing)
+
+  const transaction = web3.eth.getTransaction('TxHash')
+  console.log('transaction:', transaction)
+
+  // checkTransactionCount allows you to check for blocks in a ranges
+  function checkTransactionCount (startBlockNumber, endBlockNumber) {
+    console.log('Searching for non-zero transaction counts between blocks ' + startBlockNumber + ' and ' + endBlockNumber)
+
+    for (let i = startBlockNumber; i <= endBlockNumber; i++) {
+      const block = web3.eth.getBlock(i)
+      if (block && block.transactions && block.transactions.length) {
+        console.log('Block #' + i + ' has ' + block.transactions.length + ' transactions')
+      } else {
+        console.log('none', i)
+      }
+    }
+  }
+})(window.Web3)
